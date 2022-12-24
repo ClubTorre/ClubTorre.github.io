7bdf9361d449bbccc067c863cd01f92bd430bc69
ClubTorre <contactoclubtorre@gmail.com>
2022-12-23 21:13:47 -0600

    NAv2

---
+++ layouts/nav.php
@@ -0,0 +1,12 @@
+<header>
+        <nav class="navigation">
+            <div class="logo"><a class="inicio" href="index.html"><h1>Modas Yesenia</h1> </a></div>
+            <ul class="containerlista">
+                <li class="objetoslista"><a class="linkobjeto" href="#">Inicio</a> </li>
+                <li class="objetoslista"><a class="linkobjeto" href="#">Horarios y Costos</a></li>
+                <li class="objetoslista"><a class="linkobjeto" href="#">Torneoss</a></li>
+                <li class="objetoslista"><a class="linkobjeto" href="#">Nosotros</a></li>
+                <li class="objetoslista"><a class="linkobjeto" href="#">Nosotros</a></li>
+            </ul>
+        </nav>
+    </header>
