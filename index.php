f0755799a7f056dd6ae831e73953a1c7db4cd98b
ClubTorre <contactoclubtorre@gmail.com>
2022-12-23 19:52:32 -0600

    PHP

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
-            <title>Club Torre | Muy Pronto!</title>
+            <title>Club Torre | Muy Pronto<?php echo "!"?></title>
             <link rel="stylesheet" media="(min-width: 601px)" href="desktop.css">
             <link rel="stylesheet" media="(max-width: 600px)" href="mobile.css">
             <link rel="icon" href="./clubTorre.png">
